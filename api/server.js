8c349ea6bc656ba18c69492d5691d43a5544bbbd
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-03 12:36:40 -0600

    set up server and exported

--- api/server.js
+++ api/server.js
@@ -2,3 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send("Spell list generator back end.")
+});
+
+module.exports = server
