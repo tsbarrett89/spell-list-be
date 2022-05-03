159f79dbf8b05897e51448d0f42fc959457d7ed5
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-03 13:55:41 -0600

    character table created in up

--- data/migrations/20220503193900_character.js
+++ data/migrations/20220503193900_character.js
@@ -1,15 +1,28 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+const { defaultConfiguration } = require("express/lib/application");
+
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('character', tbl => {
+        tbl.increments()
+        tbl.string('name')
+            .notNullable()
+        tbl.string('class')
+            .notNullable()
+        tbl.integer('level')
+            .notNullable()
+        tbl.string('multiclass')
+            defaultTo("")
+        tbl.integer('multiclass_level')
+            defaultTo(0)
+        tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('user')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
   
 };
