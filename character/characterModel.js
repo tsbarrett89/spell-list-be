895dc78f4d0a5a8b2c26670ba0584efb237245f7
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-03 14:39:47 -0600

    update function created

--- character/characterModel.js
+++ character/characterModel.js
@@ -14,3 +14,7 @@ function add(details){
 function findByUser(userId){
     return db('character').where({ 'user_id': userId})
 }
+
+function update(details, id){
+    return db('character').where({ 'id': id }).update(details)
+}
