a86d2685d03c46ebbca23ff060b6a80e60f59a2b
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-03 14:34:53 -0600

    remove function created

--- user/userModel.js
+++ user/userModel.js
@@ -18,3 +18,7 @@ function findUserByEmail(email){
 function update(details, id){
     return db('user').where({ 'id': id }).update(details)
 }
+
+function remove(id){
+    return db('user').where({ 'id': id }).del()
+}
