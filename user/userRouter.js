a3a6ed4584e8d120b4c074ff822522703c772961
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-04 08:55:04 -0600

    router created with express and exported

--- user/userRouter.js
+++ user/userRouter.js
@@ -4,3 +4,8 @@ const express = require('express');
 const generateToken = require('../utils/generateToken.js');
 
 const user = require('./userModel.js');
+
+const router = express.Router()
+
+
+module.exports = router
